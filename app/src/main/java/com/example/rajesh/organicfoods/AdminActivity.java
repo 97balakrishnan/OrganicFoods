acbe8d9efc723c5756e10b32c213578772546672
balakrishnan <97balakrishnan@gmail.com>
2017-06-24 12:46:54 +0530

    check

--- app/src/main/java/com/example/rajesh/organicfoods/AdminActivity.java
+++ app/src/main/java/com/example/rajesh/organicfoods/AdminActivity.java
@@ -43,13 +43,14 @@ public class AdminActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.hide();/*
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
